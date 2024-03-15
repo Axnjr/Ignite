72e3340d59d536f6d905651b8f1518bab98206b1
Yakshit chhipa <yakshitchhipa@gmail.com>
2024-03-15 07:53:51 +0530

    progress: ws server

--- WebsocketServer/src/main.rs
+++ WebsocketServer/src/main.rs
@@ -1,53 +1,81 @@
-use axum::{ routing::{ post, get } , Json };
-use socketioxide::{ extract::{ Data, SocketRef } , SocketIo };
+use axum::{
+    routing::{get, post},
+    Error, Json,
+};
+use socketioxide::{
+    extract::{Data, SocketRef, AckSender, State},
+    SocketIo,
+    
+    
+};
 
+use dotenv::dotenv;
 use serde::Deserialize;
-
-use serde_json::{ json , Value };
-use axum::extract::{ Path , Query , State };
-
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
+use std::env;
 use tower::ServiceBuilder;
 use tower_http::cors::CorsLayer;
 
-async fn on_connect(socket: SocketRef) {
-    info!("socket connected: {}", socket.id);
+#[derive(Debug, Deserialize)]
+struct MyAuthData {
+    token: String,
+}
 
-    socket.on("JOIN", |socket: SocketRef, Data::<String>(room)| async move {
-        info!("Received join: {:?}", room);
-        let _ = socket.leave_all();
-        let _ = socket.join(room.clone());
-        socket.emit("OK", format!("Subscribed to group: {}", room))
-    });
+async fn on_connect(socket: SocketRef, Data(auth): Data<MyAuthData>, db_sate: State<Pool<Postgres>>  ) {
+
+    match 
+        sqlx::query(&format!(r#" SELECT * FROM userkeystatus WHERE apiKey = '{}'; "#, auth.token))
+        .fetch_one(&*db_sate)
+        .await 
+    {
+        Ok(_) => {
+
+            let _ = socket.emit("OK", "Server Connected !");
+
+            // socket.on("JOIN", |socket: SocketRef, Data::<String>(room)| {
+            //     println!("Received join: {:?}", room);
+            //     let _ = socket.leave_all();
+            //     let _ = socket.join(room.clone());
+            // });
+        }
+
+        Err(_) => {
+            let _ = socket.disconnect();
+        }
+    };
+        
 }
 
 #[tokio::main]
-async fn main() -> Result<(), _> {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
-    let (layer, io) = SocketIo::new_layer();
+    dotenv().ok();
+
+    let url = env::var("DB_URL").expect("DB Connection URL not found !!");
+    let db_client = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&url)
+        .await
+        .expect("DB CONNECTION FAILED")
+    ;
+
+    let (layer, io) = SocketIo::builder().with_state(db_client).build_layer();
 
     io.ns("/", on_connect);
 
-    let shared_state = io.clone();
+    // let shared_state = io.clone();
 
     let app = axum::Router::new()
-        .route("/ignite", post(req_handler))
-        // .with_state(io)
-        .layer(
-            ServiceBuilder::new()
-                .layer(CorsLayer::permissive())
-                // .layer(layer)
-            ,
-        )
+        // .route("/ignite", post(req_handler))
+        .with_state(io)
+        .layer(ServiceBuilder::new().layer(CorsLayer::permissive()))
+        .layer(layer)
     ;
 
-    info!("Starting server");
+    println!("Starting server");
 
-    axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
-        .serve(app.into_make_service())
-        .await?
-    ;
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 
     Ok(())
-
 }
-
