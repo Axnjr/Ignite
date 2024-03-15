d2998605fa706a6641557a4a0c914ab2ca3c1345
Yakshit chhipa <yakshitchhipa@gmail.com>
2024-03-15 09:53:11 +0530

    test: ws connection

--- WebsocketServer/src/main.rs
+++ WebsocketServer/src/main.rs
@@ -1,12 +1,6 @@
-use axum::{
-    routing::{get, post},
-    Error, Json,
-};
 use socketioxide::{
-    extract::{Data, SocketRef, AckSender, State},
+    extract::{AckSender, Data, SocketRef, State},
     SocketIo,
-    
-    
 };
 
 use dotenv::dotenv;
@@ -21,34 +15,33 @@ struct MyAuthData {
     token: String,
 }
 
-async fn on_connect(socket: SocketRef, Data(auth): Data<MyAuthData>, db_sate: State<Pool<Postgres>>  ) {
+async fn on_connect( socket: SocketRef, Data(auth): Data<MyAuthData>, db_sate: State<Pool<Postgres>>, ) {
 
-    match 
-        sqlx::query(&format!(r#" SELECT * FROM userkeystatus WHERE apiKey = '{}'; "#, auth.token))
-        .fetch_one(&*db_sate)
-        .await 
-    {
-        Ok(_) => {
+    let resp = sqlx::query(
+        &format!(r#" SELECT * FROM userkeystatus WHERE apiKey = '{}'; "#, auth.token )).fetch_one(&*db_sate).await
+    ;
 
-            let _ = socket.emit("OK", "Server Connected !");
+    // if resp.is_err() {
+    //     let _ = socket.emit("ERROR", "Invalid API Key");
+    //     let _ = socket.disconnect();
+    //     println!("User with Invalid API Key made a request ❌😐🤨");
+    //     return;
+    // }
+    
+    let _ = socket.emit("OK", "Server Connected !");
+    let _ = socket.emit("JOINED", "SS3DFRTGYHUJIDFGH JK");
+   
+    socket.on("JOIN", |socket: SocketRef, Data::<String>(room)| async move {
+        println!("Received join: {:?}", room);
+        let _ = socket.leave_all();
+        let _ = socket.join(room.clone());
+        let _ = socket.emit("JOINED", format!("Joined the group: {}", room));
+    });
 
-            // socket.on("JOIN", |socket: SocketRef, Data::<String>(room)| {
-            //     println!("Received join: {:?}", room);
-            //     let _ = socket.leave_all();
-            //     let _ = socket.join(room.clone());
-            // });
-        }
-
-        Err(_) => {
-            let _ = socket.disconnect();
-        }
-    };
-        
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-
     dotenv().ok();
 
     let url = env::var("DB_URL").expect("DB Connection URL not found !!");
@@ -56,8 +49,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .max_connections(5)
         .connect(&url)
         .await
-        .expect("DB CONNECTION FAILED")
-    ;
+        .expect("DB CONNECTION FAILED");
 
     let (layer, io) = SocketIo::builder().with_state(db_client).build_layer();
 
@@ -69,8 +61,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         // .route("/ignite", post(req_handler))
         .with_state(io)
         .layer(ServiceBuilder::new().layer(CorsLayer::permissive()))
-        .layer(layer)
-    ;
+        .layer(layer);
 
     println!("Starting server");
 
