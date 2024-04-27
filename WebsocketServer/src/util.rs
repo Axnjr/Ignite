ac81ec30a842e4b18724cb7f8a2e317ef80bf9d2
Yakshit Chhipa <yakshitchhipa@gmail.com>
2024-04-27 11:29:28 +0530

    depercate: util func

--- WebsocketServer/src/util.rs
+++ WebsocketServer/src/util.rs
@@ -10,15 +10,6 @@ pub fn get_request_limit_from_plan_name(val: &str) -> i64 {
     }
 }
 
-pub fn get_connection_limit_from_plan_name(val: &str) -> i64 {
-    match val {
-        "Hobby" => 10,
-        "Pro" => 500,
-        "StartUp" => 2000,
-        _ => 0,
-    }
-}
-
 pub fn devlog(val: &str) {
     dotenv().ok();
     let mode = env::var("MODE")
