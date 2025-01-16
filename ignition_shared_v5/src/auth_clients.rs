4d643f82bc14be5c1c8edf6f683d25a4ad043edf
Yakshit Chhipa <yakshitchhipa@gmail.com>
2025-01-16 05:44:30 +0530

    feat: enhance authentication logic to manage concurrent connections and user limits

--- ignition_shared_v5/src/auth_clients.rs
+++ ignition_shared_v5/src/auth_clients.rs
@@ -1,35 +1,41 @@
 use socketioxide::extract::SocketRef;
-
 use sqlx::{Pool, Postgres, Row};
-
-use crate::dashmap::{add_user_to_hash, decrement_user_hits, get_user_from_hash, increment_user_connections};
-
+use crate::dashmap::{add_user_to_hash, decrement_user_hits, get_user_from_hash};
 use crate::structs::UserLimits; 
+use crate::util::{get_connection_limit_from_plan_name, get_request_limit_from_plan_name};
 
-use crate::util::get_request_limit_from_plan_name;
+pub async fn authenticate_test_clients(socket: SocketRef, auth: &str, connections: usize){
 
-pub async fn authenticate_test_clients(socket: SocketRef, auth: String){
+    if let Some(mut user) = get_user_from_hash(auth) {
 
-    if let Some(user) = get_user_from_hash(&auth) {
+        if connections > get_connection_limit_from_plan_name(&user.plan) {
+            let _ = socket.emit("ERROR", "Con-current connection Limit Reached !");
+            let _ = socket.disconnect();
+            return;
+        }
+
         if user.hits <= 0 {
             let _ = socket.emit("ERROR", "💥 Daily request Limit Reached 🥹");
             let _ = socket.disconnect();
             return;
         }
-        increment_user_connections(&auth);
-        decrement_user_hits(&auth);
+
+        // increment_user_connections(auth);
+        user.connections = connections;
+        decrement_user_hits(auth, Some(user));
     } 
 
     else {
-        add_user_to_hash(auth.clone(), UserLimits {
+        add_user_to_hash(auth, UserLimits {
             hits: 100,
-            connections: 10,
+            connections: 1,
             plan: "Hobby".to_owned(),
+            // lru_pos: usize::MIN
         })
     }
 }
 
-pub async fn authenticate_clients(socket: SocketRef, auth: String, db_sate: Pool<Postgres>) {
+pub async fn authenticate_clients(socket: SocketRef, auth: &str, connections: usize, db_sate: Pool<Postgres>) {
 
     // - Check if user already exists in the hashmap 
     //      - If yes, 
@@ -41,15 +47,25 @@ pub async fn authenticate_clients(socket: SocketRef, auth: String, db_sate: Pool
     //      - if yes, add the user to the hashmap
     //      - if no, disconnect the user
    
-    if let Some(user) = get_user_from_hash(&auth) {
+    if let Some(mut user) = get_user_from_hash(auth) {
+
+        if connections > get_connection_limit_from_plan_name(&user.plan) {
+            let _ = socket.emit("ERROR", "Con-current connection Limit Reached !");
+            let _ = socket.disconnect();
+            return;
+        }
+
         if user.hits <= 0 {
             let _ = socket.emit("ERROR", "💥 Daily request Limit Reached 🥹");
             let _ = socket.disconnect();
             return;
         }
-        increment_user_connections(&auth);
-        decrement_user_hits(&auth);
-    } 
+
+        // update the concurrent connection count for the user with given key / client_id
+        user.connections = connections;
+        decrement_user_hits(auth, Some(user));
+    }
+     
 
     else {
         match sqlx::query(&format!(r#" SELECT * FROM userkeystatus WHERE apiKey = '{}'; "#, auth))
@@ -57,15 +73,20 @@ pub async fn authenticate_clients(socket: SocketRef, auth: String, db_sate: Pool
             .await
         {
             Ok(resp) => {
-                let p = resp.try_get::<String, _>("plantype").unwrap();
+
+                let user_plan = resp.try_get::<String, _>("plantype").unwrap();
+
                 let u = UserLimits {
-                    hits: get_request_limit_from_plan_name(&p),
+                    hits: get_request_limit_from_plan_name(&user_plan),
                     connections: 1,
-                    plan: p.to_string(),
+                    plan: user_plan.to_string(),
+                    // lru_pos: usize::MIN,
                 };
-                add_user_to_hash(auth.clone(), u);
+
+                add_user_to_hash(auth, u);
                 println!("User with API Key: {} added to the hashmap ✌️", auth);
             }
+
             Err(_err) => {
                 let _ = socket.emit("ERROR", "Invalid API Key");
                 let _ = socket.disconnect();
