3962b33f3ec90c95b1f15c446fe81e39e7134ee8
Yakshit Chhipa <yakshitchhipa@gmail.com>
2025-01-16 05:33:54 +0530

    feat: update request limit functions to use usize for improved memory handling

--- ignition_shared_v5/src/util.rs
+++ ignition_shared_v5/src/util.rs
@@ -1,11 +1,20 @@
 use std::env;
 use dotenv::dotenv;
 
-pub fn get_request_limit_from_plan_name(val: &str) -> i64 {
+pub fn get_request_limit_from_plan_name(val: &str) -> usize {
     match val {
         "Hobby" => 500,
         "Pro" => 1000000,
         "StartUp" => 5000000,
+        _ => 0,
+    }
+}
+
+pub fn get_connection_limit_from_plan_name(val: &str) -> usize {
+    match val {
+        "Hobby" => 10,
+        "Pro" => 500,
+        "StartUp" => 5000,
         _ => 0,
     }
 }
