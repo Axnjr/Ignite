12a4373bdb28534e3bb47adb9c2da08ccf42eba3
Yakshit Chhipa <yakshitchhipa@gmail.com>
2025-01-16 05:45:29 +0530

    refactor: reorganize module imports and enhance configuration management for improved clarity

--- ignition_shared_v5/src/main.rs
+++ ignition_shared_v5/src/main.rs
@@ -1,163 +1,95 @@
-mod map;
-mod structs;
-mod util;
-mod handlers;
+// mod map;
 mod auth_clients;
+mod dashmap;
+mod handlers;
+mod logging;
+mod macros;
+mod structs;
+mod tcp;
+mod lru;
+mod util;
+mod io_register;
 
-use axum::{extract::Query, http::Method};
+use dashmap::init_map;
+use io_register::register_ws_io_handlers;
+use tcp::initialize_tcp_router;
+use logging::{log_and_panic, log_messages_to_log_file};
 
-use axum::routing::get;
-
+use tokio::signal;
 use tracing_subscriber::FmtSubscriber;
-
-use socketioxide::{ 
-    extract::{
-        AckSender, 
-        Data, 
-        SocketRef
-    }, 
-    SocketIo 
-};
-
+use socketioxide::SocketIo;
 use sqlx::postgres::PgPoolOptions;
-
 use dotenv::dotenv;
+use std::{env, net::SocketAddr};
 
-use structs::{
-    JoinLeaveRequestData, 
-    MyAuthData,
-    UpgradeKey
-};
+struct Config {
+    db_url: String,
+    max_connections: u32,
+    port: u16,
+}
 
-use map::{
-    remove_user_from_hash, 
-    get_user_hash_map,
-    init_map,
-    reset_all_users_values
-};
-
-use std::{
-    env, 
-    net::SocketAddr
-};
-
-use tower::ServiceBuilder;
-
-use tower_http::cors::{Any, CorsLayer};
-
-use crate::handlers::info_handler;
-
-use crate::{
-    auth_clients::authenticate_clients, 
-    handlers::{
-        join_handler, 
-        leave_handler, 
-        message_handler
+impl Config {
+    fn from_env() -> Result<Self, Box<dyn std::error::Error>> {
+        dotenv().ok(); // Load .env variables
+        Ok(Self {
+            db_url: env::var("DB_URL")?,
+            port: env::var("PORT")
+                .unwrap_or_else(|_| "3000".to_string())
+                .parse()?,
+            max_connections: env::var("MAX_DB_CONNECTIONS")?.parse().unwrap(),
+        })
     }
-};
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
-    dotenv().ok();
-
     init_map();
+
+    let server_config = log_if_panic!(
+        Config::from_env, 
+        "Error reading enviroment variables !!"
+    );
 
     let subscriber = FmtSubscriber::new();
     tracing::subscriber::set_global_default(subscriber)?;
 
-    let url = env::var("DB_URL").expect("DB Connection URL not found ☠️❌😬😱");
+    let db_client = log_if_panic_async!(
+        PgPoolOptions::new()
+            .max_connections(server_config.max_connections)
+            .connect(server_config.db_url.as_str()),
+        "Failed to connect to the database. Check the connection URL."
+    );
 
-    let db_client = PgPoolOptions::new()
-        .max_connections(5)
-        .connect(&url)
-        .await
-        .expect("DB CONNECTION FAILED ☠️❌😬😱");
+    // let (layer, io) =  SocketIo::builder().with_state(db_client).build_layer();
+    let db                   = db_client.clone();
+    let (layer, io) = SocketIo::new_layer();
 
-    let db = db_client.clone();
+    register_ws_io_handlers(&io, db);
 
-    let (layer, io) = SocketIo::builder().with_state(db_client).build_layer();
-
-    io.ns("/", |s: SocketRef, Data::<MyAuthData>(auth)| async move { //  Data::<MyAuthData>(auth)
-
-        let _ = s.emit("LOG", "PING");
-        // ----- Test event ----- //
-        //      s.on("client to server event", |s: SocketRef| {
-        //          let _ = s.emit("server to client event", "Received client to server event");
-        //      });
-        // ----- Test event ----- //
-
-        // Register a handler for the "INFO" event
-        s.on("INFO", |Data::<MyAuthData>(payload), ack: AckSender| async move {
-            info_handler(payload, ack);
-        });
-
-        // Register a handler for the "MESSAGE" event
-        s.on("MESSAGE", message_handler);
-
-        // Register a handler for the "LEAVE" event
-        s.on("LEAVE", leave_handler);   
-
-        // Register a handler for the "JOIN" event
-        s.on("JOIN", |s: SocketRef, Data::<JoinLeaveRequestData>(payload), ack: AckSender| async move {
-            join_handler(s, payload, ack);
-        });       
-
-        // Authenticate the client with their "auth.token"
-        authenticate_clients(s, auth.token, db).await;
-
-    });
-
-    let _cors = CorsLayer::new()
-        .allow_methods([Method::GET, Method::POST])
-        .allow_origin(Any)
-        .allow_headers(Any)
-    ;
-
-    let app = axum::Router::new()
-
-        .with_state(io)
-
-        .route(
-            "/users",
-            get(|| async move {
-                println!("Test Route");
-                get_user_hash_map().unwrap()
-            })
-        )
-
-        .route(
-            "/reset",
-            get(|| async move {
-                    reset_all_users_values();
-                    "MAP RESETED !!"
-                }
-            )
-        )
-
-        .route(
-            "/upgrade", 
-            get(|Query(params): Query<UpgradeKey>| async move { 
-                remove_user_from_hash(&params.key);
-        }))
-
-        .layer(
-            ServiceBuilder::new()
-            .layer(CorsLayer::permissive())
-            .layer(layer),
-        );
-
-    let port = std::env::var("PORT")
-        .ok()
-        .and_then(|s| s.parse().ok())
-        .unwrap_or(3000)
-    ;
-
-    let address = SocketAddr::from(([0, 0, 0, 0], port));
-    println!("Server:v5 Started On Port: {}", address);
-
+    let app           = initialize_tcp_router(io, layer);
+    let address   = SocketAddr::from(([0, 0, 0, 0], server_config.port));
     let listener = tokio::net::TcpListener::bind(&address).await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+
+    log_message!("INFO", "Server:v5 Started On Port: {}", address);
+
+    axum::serve(listener, app)
+        .with_graceful_shutdown(shutdown_signal())
+        .await
+        .unwrap()
+    ;
 
     Ok(())
+}
+
+
+async fn shutdown_signal() {
+
+    // STOPED SERVER FROM TERMINAL USING Ctrl+C
+
+    signal::ctrl_c()
+        .await
+        .expect("Failed to listen for shutdown signal");
+
+    log_message!("DEBUG", "Server stopped due to Ctrl+c signal.");
 }
